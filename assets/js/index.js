6b3fc19c23faa53a8c79ca704704fccac591b42d
Christian Kreiling <ckreiling@IMPRIVATA.COM>
2017-06-02 14:52:33 -0400

    This commit is the final boilerplate for the project. This already has a login system with User and Group objects, and a simple API. Go to the \music folder and run "python manage.py runserver", then in your browser visit localhost:8000/system/index and you will see a list of users in the database. In assets/js/index.js you find the single React component generating that HTML, polling the server for more users. This is powerful, because users will need to continually poll the system for updated voting records until a Websocket server is setup alongside the RESTful API in order to broadcast changes.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,45 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Hello = React.createClass({
+var UserList = React.createClass({
+  loadUsersFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      datatype: 'json',
+      cache: false,
+      success: function(data){
+        console.log('Upon success, the data being received is: ' + data);
+        this.setState({data: data});
+      }.bind(this)
+    })
+  },
+
+  getInitialState: function() {
+    return {data: null};
+  },
+
+  componentDidMount: function() {
+    this.loadUsersFromServer();
+    setInterval(this.loadUsersFromServer, this.props.pollInterval);
+  },
+
   render: function () {
+    if (this.state.data) {
+      console.log('Users retrieved');
+      console.log(this.state.data);
+      var userNodes = this.state.data.results.map(function(user) {
+        return (<li> {user.username} </li>);
+      });
+    }
     return (
-      <h1>
-        Hello, React!
-      </h1>
+      <div>
+        <h1>Hey, React! Here's some users:</h1>
+        <ul>
+          {userNodes}
+        </ul>
+      </div>
     );
   }
 });
 
-ReactDOM.render(<Hello />, document.getElementById('container'));
+ReactDOM.render(<UserList url="/system/users.json" pollInterval={1000} />, document.getElementById('container'));
